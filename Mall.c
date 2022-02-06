af7cb165f47bb789fcdce5b16a9debbb639d6c6e
samarth <techiedost1@gmail.com>
2022-02-06 18:57:19 +0530

    Initialization

--- Mall.c
+++ Mall.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
+void show_bank_balance();
 int main(){
-    printf("Hello World\n");
+    int choice=0;
+    printf("Welcome to The Mall\nEnter Your Choice\n1.View Bank Balance\n");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+    case 1:
+        show_bank_balance();
+        break;
+    
+    default:
+        printf("Wrong choice");
+        break;
+    }
 }
---
+++ samarth.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+Functions
+void show_bank_balance()
+*/
+void show_bank_balance(){
+    float balance=100;
+    printf("Your Balance : %f\n",balance);
+}
